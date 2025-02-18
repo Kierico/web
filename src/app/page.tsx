3ab4d810cd801ecadc25445a2454ec17071b78d5
Kiérico <kiericograndao@gmail.com>
2025-02-18 18:39:16 -0300

    Pattern of composition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,27 @@
-import {ArrowRight, Copy} from 'lucide-react'
+import {ArrowRight, Copy, Mail} from 'lucide-react'
 import {Button} from '@/components/button'
 import { IconButton } from '@/components/icon-button';
+import { InputField, InputIcon, InputRoot } from '@/components/input';
 
 export default function Home() {
   return (
     <main>
 
-      <Button>
+      <Button type='submit'>
         Enviar
         <ArrowRight />
       </Button>
       <IconButton>
         <Copy />
       </IconButton>
+      <div>
+        <InputRoot>
+          <InputIcon>
+            <Mail className='size-5'/>
+          </InputIcon>
+          <InputField placeholder='Coloque seu e-mail'/>
+        </InputRoot>
+      </div>
       
     </main>
   );
