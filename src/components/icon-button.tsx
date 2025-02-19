5c94ee4cf854ef96308558568b1643b2eb0be8ee
Kiérico <kiericograndao@gmail.com>
2025-02-19 15:39:15 -0300

    Setting copy (clipboard) of invite

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -7,14 +7,12 @@ interface IconButtonProps extends ComponentProps<'button'> {
 
 export function IconButton({ className, ...props }: IconButtonProps) {
   return (
-    // biome-ignore lint/a11y/useButtonType: <explanation>
     <button
       className={twMerge(
         'p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900',
         className
       )}
-    >
-      {props.children}
-    </button>
+      {...props}
+    />
   )
 }
